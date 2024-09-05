caf29676498d36f12c712741258d9b2ebab2fac4
MD Shafiul Islam <shafiul2014bd@gmail.com>
2024-09-05 20:58:02 +0600

    update cros

--- src/app.js
+++ src/app.js
@@ -20,13 +20,19 @@ class App {
   }
 
   initializeMiddlewares = () => {
-    this.expressApp.use(cors());
+    const corsConfig = {
+      origin: ["**", "https://ayat-art-craft.web.app", "https://ayat-art-craft.firebaseapp.com/**",],
+      credentials: true,
+      "Access-Control-Allow-Origin": "*",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+    };
+
+    this.expressApp.options("", cors(corsConfig));
     this.expressApp.use(express.urlencoded({ extended: false }));
     this.expressApp.use(express.json({ limit: "20mb" }));
   };
 
   connectToDatabase = async () => {
-    //mongodb+srv://md_shafiul:ukmoVJzlpbCUnoqE@cluster0.rdwf6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 
     const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rdwf6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
   };
